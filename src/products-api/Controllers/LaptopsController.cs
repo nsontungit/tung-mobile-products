01bcf0ed08cb892227bd5ea5b2c269f7bba62ec6
nstung7 <97228470+Nguyen-Son-Tung@users.noreply.github.com>
2022-08-28 23:52:58 +0700

    Faking data

---
+++ src/products-api/Controllers/LaptopsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using products_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace products_api.Controllers
+{
+    [Route("api/laptops")]
+    [ApiController]
+    public class LaptopsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOne()
+        {
+            var result = new
+            {
+                Name = "Thinkpad3",
+                Ram = "16GB",
+                Rom = "256GB",
+                Price = 223.22
+            };
+            return Ok(ApiResultCreator.Create(result));
+        }
+    }
+}
---
+++ src/products-api/Models/ApiResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace products_api.Models
+{
+    public class ApiResult
+    {
+        public object Data { get; set; }
+        public string Message { get; set; }
+    }
+    public static class ApiResultCreator
+    {
+        public static ApiResult Create(object data, string message = null)
+        {
+            return new ApiResult
+            {
+                Data = data,
+                Message = message
+            };
+        }
+    }
+}
